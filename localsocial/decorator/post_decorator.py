14bbfecd93f5ab4abc773105b2c462658a422978
Bill Liu <liu_bill@live.com>
2015-11-24 01:20:04 -0600

    Allowing changing status code for api endpoint decorator

--- localsocial/decorator/post_decorator.py
+++ localsocial/decorator/post_decorator.py
@@ -12,7 +12,15 @@ def api_endpoint(rule, **options):
 		def new_function(*args, **kwargs):
 			endpoint = options.pop('endpoint', None)
 
-			json_dict_result = original_function(*args, **kwargs)
+			original_response = original_function(*args, **kwargs)
+
+			if(isinstance(original_response, tuple)):
+				json_dict_result = original_response[0]
+				status_code = original_response[1]
+			else:
+				json_dict_result = original_response
+				status_code = 200
+
 
 			stringified_json = ""
 			if request.args.get("pretty") == "true":
@@ -20,10 +28,7 @@ def api_endpoint(rule, **options):
 			else:
 				stringified_json = json.dumps(json_dict_result)
 
-			print("HELLLO!")
-			print(stringified_json)
-
-			new_response = make_response(stringified_json)
+			new_response = make_response(stringified_json, status_code)
 			new_response.mimetype = "application/json"
 			
 			return new_response
