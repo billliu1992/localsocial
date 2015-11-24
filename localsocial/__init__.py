5f7e757e0fdf453b38316d5ad6672a88f5219a1d
Bill Liu <liu_bill@live.com>
2015-11-24 01:18:49 -0600

    Changing static file folder location, removing caching, for development of static files

--- localsocial/__init__.py
+++ localsocial/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, instance_relative_config=True, static_url_path="/web", static_folder="../public/")
+app = Flask(__name__, instance_relative_config=True, static_url_path="/web", static_folder="../public/target")
 
 app.config.from_pyfile('config.py')
+
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
 
 from localsocial.routes import user_routes
