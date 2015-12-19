5676254fc3abaa13b14c5b0cd25b56eb1af91f30
Bill Liu <liu_bill@live.com>
2015-12-19 17:21:41 -0600

    Refactoring post-service code to be cleaner

--- public/app/components/post-service.js
+++ public/app/components/post-service.js
@@ -9,35 +9,40 @@ define(['axios', 'components/coordinates-model'], function(axios, Coordinates) {
 		}
 
 		return builtString.substring(0, builtString.length - 1);
-	}
+	};
+
+	var degreesToRadians = function(degrees) {
+		return degrees * (Math.PI / 180);
+	};
 
 	var PostService = {
 		getPosts() {
-			return new Promise((resolve, reject) => {
-				axios.get('/post')
-					.then(response => resolve(response.data))
-					.catch(response => reject({ status: response.status, data: response.data }));
-			});
+			return axios.get('/post')
+				.then((response) => response.data)
+				.catch((response) => ({ status: response.status, data: response.data }));
 		},
 
 		savePost(data) {
-			return new Promise((resolve, reject) => {
-				axios.post('/post', transformObjectToForm(data), {
+			return axios.post('/post', transformObjectToForm(data), {
 					headers : {
 						'Content-Type' : 'application/x-www-form-urlencoded'
 					}
 				})
-				.then((response) => resolve(response.data))
-				.catch((response) => reject({status: response.status, data: response.data }));
-			});
+				.then((response) => response.data)
+				.catch((response) => ({ status: response.status, data: response.data }));
 		},
 
 		// Distance approximation: http://www.movable-type.co.uk/scripts/latlong.html
 		getDistance(coord1, coord2, isMetric) {
 			var radius = isMetric ? 6371 /* meters */ : 3959 /* miles */;
+
+			var lat1 = degreesToRadians(coord1.latitude);
+			var long1 = degreesToRadians(coord1.longitude);
+			var lat2 = degreesToRadians(coord2.latitude);
+			var long2 = degreesToRadians(coord2.longitude);
  
-			var x = (coord2.longitude - coord1.longitude) * Math.cos((coord2.latitude + coord1.longitude) / 2);
-			var y = coord2.latitude - coord1.longitude;
+			var x = (long2 - long1) * Math.cos((lat2 + lat1) / 2);
+			var y = lat2 - lat1;
 
 			return Math.sqrt(x*x + y*y) * radius;
 		}
