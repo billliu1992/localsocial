ab99fe6c7ca406613bdb65fb1dc98abb3c78b178
Bill Liu <liu_bill@live.com>
2016-04-23 22:45:00 -0500

    Finish post service unit test

--- localsocial/service/post_service.py
+++ localsocial/service/post_service.py
@@ -1,11 +1,21 @@
+"""
+Service for operations on posts, including replies and likes
+"""
+
 import json
 
 from datetime import datetime
+from localsocial.service.util import page_to_query
 from localsocial.database import post_dao, reply_dao, like_dao
 from localsocial.model.post_model import Post, EventPost, ImagePost
 from localsocial.model.reply_model import Reply
 
 def create_new_post(author, body, privacy, location):
+	"""
+	Create a new post and persist it
+
+	Returns the persisted post
+	"""
 	current_time = datetime.now()
 
 	new_post = Post(author.user_id, author.full_name, author.portrait, body, current_time, privacy, location, 0, False)
@@ -15,6 +25,11 @@ def create_new_post(author, body, privacy, location):
 	return new_post
 
 def create_new_reply(author, body, parent_post_id, location, privacy):
+	"""
+	Create a new reply and persist it
+
+	Returns the persisted reply
+	"""
 	current_time = datetime.now()
 
 	new_reply = Reply(parent_post_id, author.user_id, "", author.portrait, body, 
@@ -25,35 +40,71 @@ def create_new_reply(author, body, parent_post_id, location, privacy):
 	return new_reply
 
 def delete_post_by_id(current_user_id, post_id):
-	# Delete dangling rows
+	"""
+	Delete the post with the post_id and current_user_id. The current_user_id
+	is used for validation.
+
+	Return True if successful
+	"""
 	reply_dao.delete_replies_by_post_id(post_id)
 	like_dao.delete_likes(post_id)
 
 	return post_dao.delete_post_by_id(post_id, current_user_id) > 0
 
 def delete_reply_by_id(current_user_id, reply_id):
+	"""
+	Delete the reply with the post_id and current_user_id. The current_user_id
+	is used for validation.
+
+	Return True if successful
+	"""
 	return reply_dao.delete_reply_by_id(reply_id, current_user_id)
 
 def get_posts_by_user(current_user_id, searched_user, are_friends, page_num=1, post_per_page=10, max_id=None):
-	if page_num < 1:
-		page_num = 1
+	"""
+	Get all the posts with by searched_user. Post visibility is determined by current_user_id and are_friends.
+	Parameters can be passed in for pagination.
 
-	return post_dao.get_posts_by_user(current_user_id, searched_user, are_friends, post_per_page, (page_num - 1) * post_per_page, max_id)
+	Returns a list of the queried post
+	"""
+	limit = page_to_query(page_num, post_per_page)
+
+	return post_dao.get_posts_by_user(current_user_id, searched_user, are_friends, limit, max_id)
 
 def get_post_by_id(current_user_id, post_id):
+	"""
+	Get a single post by the post_id. Visiblity is determined by current_user_id.
+
+	Returns the post object
+	"""
 	return post_dao.get_post_by_id(current_user_id, post_id)
 
 def get_post_feed(current_user_id, location, max_dist=25, page_num=1, post_per_page=10, max_id = None):
-	if(page_num < 1):
-		page_num = 1
+	"""
+	Get the user's feed. Parameters can be passed for distance filtering and pagination.
+
+	Returns a list of posts
+	"""
+	limit = page_to_query(page_num, post_per_page)
 		
-	return post_dao.get_post_feed(current_user_id, location, max_dist, post_per_page, (page_num - 1) * post_per_page, max_id)
+	return post_dao.get_post_feed(current_user_id, location, max_dist, post_per_page, limit, max_id)
 
 def get_post_replies(post):
+	"""
+	Get all the replies to the passed in post
+
+	Returns a list of replies
+	"""
 	return reply_dao.get_replies_by_post_id(post.post_id)
 
 def create_like(post_id, author_id):
+	"""
+	Creates a like to the post as the author_id, and persist it
+	"""
 	return like_dao.create_like(post_id, author_id)
 
 def delete_like(post_id, author_id):
+	"""
+	Deletes a like to the post as the author_id, and persists it
+	"""
 	return like_dao.delete_like(post_id, author_id)
